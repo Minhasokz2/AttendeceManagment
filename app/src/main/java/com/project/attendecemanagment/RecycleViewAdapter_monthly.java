b0e8cd22228ce2e7c402337b22a247d2e5953fb1
Rashid Minhas <minhasokz2@gmail.com>
2020-01-06 23:14:04 +0500

    New Update

---
+++ app/src/main/java/com/project/attendecemanagment/RecycleViewAdapter_monthly.java
@@ -0,0 +1,4 @@
+package com.project.attendecemanagment;
+
+public class RecycleViewAdapter_monthly {
+}
---
+++ app/src/main/java/com/project/attendecemanagment/RecyleViewAdapter_daily.java
@@ -0,0 +1,4 @@
+package com.project.attendecemanagment;
+
+public class RecyleViewAdapter_daily {
+}
