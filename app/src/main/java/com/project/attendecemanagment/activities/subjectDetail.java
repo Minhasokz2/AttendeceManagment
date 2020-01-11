e5a564d96c34dde38fb402b6bbd71b7e362a84c7
Rashid Minhas <minhasokz2@gmail.com>
2020-01-11 10:21:37 +0500

    Updates

--- app/src/main/java/com/project/attendecemanagment/activities/subjectDetail.java
+++ app/src/main/java/com/project/attendecemanagment/activities/subjectDetail.java
@@ -1,23 +1,76 @@
 package com.project.attendecemanagment.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.project.attendecemanagment.R;
+import com.project.attendecemanagment.recycleview.RecycleViewAdapter_subject;
+
+import java.util.ArrayList;
 
 public class subjectDetail extends AppCompatActivity {
+
+    private ArrayList<String > subjectNames = new ArrayList<>();
+    private ArrayList<String > image = new ArrayList<>();
+    private ArrayList<String > imageSecond = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subject_detail);
+        initBitmap();
     }
 
     public void monthlyDetail(View view) {
         Intent intent = new Intent(this,monthly_attendece.class);
         startActivity(intent);
+    }
+    private  void initBitmap(){
+
+        subjectNames.add("first");
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+        subjectNames.add("second");
+
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+        subjectNames.add("third");
+
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+        subjectNames.add("forth");
+
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+
+        subjectNames.add("fifth");
+
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+        subjectNames.add("sixth");
+
+        image.add(String.valueOf(R.drawable.student));
+        imageSecond.add(String.valueOf(R.drawable.bookshelf));
+
+        intitRecylceView();
+
+    }
+    private void intitRecylceView(){
+        RecyclerView recyclerView = findViewById(R.id.recycleViewSubject);
+        RecycleViewAdapter_subject adapter_subject = new RecycleViewAdapter_subject(this,image,imageSecond,subjectNames);
+        recyclerView.setAdapter(adapter_subject);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
--- app/src/main/java/com/project/attendecemanagment/recycleview/RecycleViewAdapter_subject.java
+++ app/src/main/java/com/project/attendecemanagment/recycleview/RecycleViewAdapter_subject.java
@@ -1,4 +1,92 @@
 package com.project.attendecemanagment.recycleview;
 
-public class RecycleViewAdapter_subject {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.project.attendecemanagment.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+public class RecycleViewAdapter_subject extends RecyclerView.Adapter<RecycleViewAdapter_subject.ViewHolder> {
+
+    private  ArrayList <String> mSubjectName = new ArrayList<>();
+//    private  ArrayList <String> mImage = new ArrayList<>();
+//    private  ArrayList <String> mSecondImage = new ArrayList<>();
+    Context context;
+
+    public RecycleViewAdapter_subject(Context context,ArrayList<String> mImage, ArrayList<String> mSecondImage, ArrayList<String> mSubjectName) {
+        this.context = context;
+//        this.mImage = mImage;
+//        this.mSecondImage = mSecondImage;
+        this.mSubjectName = mSubjectName;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.subject_listview,parent,false);
+
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+        Log.d(TAG, "onBindViewHolder: called");
+
+        holder.subjectName.setText(mSubjectName.get(position));
+//        Glide.with(context).asBitmap().load(mImage.get(position)).into(holder.image);
+//        Glide.with(context).asBitmap().load(mSecondImage.get(position)).into(holder.imageSecond);
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSubjectName.size();
+    }
+
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
+//        ImageView image;
+//        ImageView imageSecond;
+        TextView subjectName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+//            image = itemView.findViewById(R.id.firstIcon);
+//            imageSecond = itemView.findViewById(R.id.secondIcon);
+            subjectName = itemView.findViewById(R.id.sunjectName);
+            parentLayout = itemView.findViewById(R.id.parentLayout);
+
+        }
+    }
+
+
 }
+
